ffd72cc37da1429a9fed626f3bb2b794a65c9334
stiffelbo <partygrouppl@gmail.com>
2021-06-13 19:21:47 +0200

    28.4 working app

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,91 @@
 import React from 'react';
+import io from 'socket.io-client';
+import uuid from 'uuid';
 
 class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {tasks: [], newTask: ''};
+  }
 
+  componentDidMount() {
+    this.socket = io('localhost:8000');
+    //get existing tasks from server
+    this.socket.on('updateTasks', (tasks) => this.updateTasks(tasks));
+    //remove task
+    this.socket.on('removeTask', (id) => this.removeTask(id));
+    //add new task
+    this.socket.on('addTask', (task) => this.addTask(task));
+  }
+
+  updateTasks(newTasks) {
+    this.setState({
+      tasks: newTasks,
+    });
+  };
+
+  addTask(newTask){
+    this.setState({
+      tasks: [...this.state.tasks, newTask],
+    });
+  }
+
+  removeTask(taskId, taskName = null){
+    this.setState({
+      tasks: this.state.tasks.filter(val => val.id !== taskId),
+    });
+    //task name works as filter to avoid infinity loop on sockets, it is only passed from component but not from server so socket emits only form user that removes task.
+    if(taskName) this.socket.emit('removeTask', taskId);
+  }
+
+  submitForm(e){
+    e.preventDefault();
+    const newTask = {
+      id: uuid.v4(),
+      name: this.state.newTask,
+    }
+    this.addTask(newTask);    
+    this.socket.emit('addTask', newTask);
+    this.setState({newTask: ''});
+  }
+
+  
   render() {
+    const {tasks, newTask} = this.state;
     return (
       <div className="App">
     
         <header>
-          <h1>ToDoList.app</h1>
+          <h1>ToDoList</h1>
         </header>
     
         <section className="tasks-section" id="tasks-section">
           <h2>Tasks</h2>
     
           <ul className="tasks-section__list" id="tasks-list">
-            <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+            {tasks.map(task => (
+              <li key={task.id} className='task'>
+                {task.name}
+                <button className="btn btn--red" onClick={() => this.removeTask(task.id, task.name)}>Remove</button>
+              </li>
+            ))}
           </ul>
-    
-          <form id="add-task-form">
-            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
+          <form 
+          id="add-task-form"
+          onSubmit={event => this.submitForm(event)}
+          >
+            <input 
+            className="text-input" 
+            autoComplete="off" 
+            type="text" 
+            placeholder="Type your description" 
+            id="task-name"
+            value={newTask} 
+            onChange={e => this.setState({newTask: e.currentTarget.value})}
+            />
             <button className="btn" type="submit">Add</button>
-          </form>
-    
+          </form>    
         </section>
       </div>
     );
--- server.js
+++ server.js
@@ -1,9 +1,55 @@
 const express = require('express');
 const path = require('path');
+const uuid = require('uuid');
 const socket = require('socket.io');
 
 const app = express();
 
+//task array
+let tasks = [
+  {id: 1, name: "Learn array functions"},
+  {id: 2, name: "Learn react components"},
+  {id: 3, name: "Learn redux"},
+  {id: 4, name: "Learn web sockets"},
+  {id: 5, name: "Learn mongo"},
+  {id: 6, name: "Learn backend tests"},
+  {id: 7, name: "Learn security"},
+];
+
+//task methods
+
+const addTask = (task) =>{  
+  tasks.push(task);
+}
+
+const removeTask = (taskId) =>{
+  tasks = tasks.filter(task => task.id !== taskId);
+}
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client')));
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on Port:', 8000)
+});
+
+//web sockets
+const io = socket(server);
+io.on('connection', (socket) => { 
+
+  socket.emit('updateTasks', tasks);
+  
+  socket.on('addTask', (task) => {    
+    addTask(task);        
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('removeTask', (id) => {     
+    removeTask(id);
+    socket.broadcast.emit('removeTask', id);
+  });
+  socket.on('disconnect', () => { 
+    const id = socket.id;
+    console.log('disconected: ', id);
+  });
+  console.log(`new client connected: ${socket.id}`);
 });
